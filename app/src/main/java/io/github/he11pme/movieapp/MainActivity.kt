159f6e5315a59cf14a2e01093d85f6c5dc3b364e
Ivan Mineev <H.E.L.P.M.E@yandex.ru>
2026-01-02 15:17:51 +0500

    feature: create app and bottom bars

--- app/src/main/java/io/github/he11pme/movieapp/MainActivity.kt
+++ app/src/main/java/io/github/he11pme/movieapp/MainActivity.kt
@@ -1,20 +1,49 @@
 package io.github.he11pme.movieapp
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import io.github.he11pme.movieapp.databinding.ActivityMainBinding
+import io.github.he11pme.movieapp.utils.extensions.doOnApplyWindowInsets
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        configureSystemBars()
+        setInsets()
+
+        setContentView(binding.root)
+    }
+
+    private fun configureSystemBars() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            binding.bottomNavigationView.background = null
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            @Suppress("DEPRECATION")
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    private fun setInsets() {
+        binding.bottomAppBar.doOnApplyWindowInsets { v, _ ->
+            v.updatePadding(bottom = 0)
+        }
+
+        binding.toolbar.doOnApplyWindowInsets { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            v.updatePadding(top = systemBars.top)
         }
     }
 }
