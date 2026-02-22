16c9a613b75f4471ad9efc44121c757927b52f69
Ivan Mineev <H.E.L.P.M.E@yandex.ru>
2026-02-22 08:09:46 +0500

    feat: add switch app theme with sharedPreferences and listener

--- app/src/main/java/io/github/he11pme/movieapp/App.kt
+++ app/src/main/java/io/github/he11pme/movieapp/App.kt
@@ -1,7 +1,37 @@
 package io.github.he11pme.movieapp
 
 import android.app.Application
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
+import io.github.he11pme.movieapp.data.local.preference.PreferenceProvider
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application()
+class App : Application() {
+
+    @Inject
+    lateinit var preferenceProvider: PreferenceProvider
+
+    private val shared by lazy { this.getSharedPreferences("app_settings", MODE_PRIVATE) }
+
+    private val defaultTheme get() = preferenceProvider.getDefaultTheme()
+
+    private val listener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            when (key) {
+                "default_theme" -> setNightMode()
+            }
+        }
+
+    override fun onCreate() {
+        super.onCreate()
+        setNightMode()
+        shared.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    private fun setNightMode() {
+        AppCompatDelegate.setDefaultNightMode(defaultTheme)
+    }
+
+}
--- app/src/main/java/io/github/he11pme/movieapp/view/fragments/settings/SettingsViewModel.kt
+++ app/src/main/java/io/github/he11pme/movieapp/view/fragments/settings/SettingsViewModel.kt
@@ -1,7 +1,21 @@
 package io.github.he11pme.movieapp.view.fragments.settings
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.he11pme.movieapp.domain.repository.PreferenceRepository
+import javax.inject.Inject
 
-class SettingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val preferenceRepository: PreferenceRepository
+) : ViewModel() {
+
+    fun saveDefaultTheme(theme: Int) {
+        preferenceRepository.saveDefaultTheme(theme)
+    }
+
+    fun getDefaultTheme(): Int {
+        return preferenceRepository.getDefaultTheme()
+    }
+
 }
